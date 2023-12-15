6a8c851d983cd2452af4c63ea44a2cb8acc7ab6e
4PTP_1 <4PTP_1@DESKTOP-HMD73UL>
2023-12-15 10:15:34 +0100

    dodanie obsługi obrazow w code behind

---
+++ ms_bb/Images.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ms_bb
+{
+    public class Images
+    {
+        public string ImgageSource { get; set; }
+        public string ImageName { get; set; }
+    }
+}
