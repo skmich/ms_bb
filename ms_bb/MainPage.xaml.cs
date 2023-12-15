a24d4cd657d3fb514bde1bc796d86abc50bf97ff
4PTP_1 <4PTP_1@DESKTOP-HMD73UL>
2023-12-15 10:34:33 +0100

    dodanie obsługi przesuwania obrazow

--- ms_bb/MainPage.xaml.cs
+++ ms_bb/MainPage.xaml.cs
@@ -11,18 +11,41 @@ namespace ms_bb
     public partial class MainPage : ContentPage
     {
         private List<Images> images;
+        private bool IsStarted = true;
+        private int actualPosition; 
         public MainPage()
         {
             InitializeComponent();
 
             images = new List<Images>
             {
-                new Images { ImageSource = "forest,jpg", ImageName="forest"},
-                new Images { ImageSource = "forest,jpg", ImageName="forest"},
-                new Images { ImageSource = "forest,jpg", ImageName="forest"},
+                new Images { ImageSource = "forest.jpg", ImageName="forest"},
+                new Images { ImageSource = "forest.jpg", ImageName="forest"},
+                new Images { ImageSource = "forest.jpg", ImageName="forest"},
             };
+            imagesCarousel.ItemsSource = images;
 
-            
+            Device.StartTimer(TimeSpan.FromSeconds(5), () =>
+            {
+                if (IsStarted)
+                {
+                    SlideImages();
+                }
+
+
+                return true;
+            });
+
+
+            void SlideImages()
+            {
+                actualPosition = (actualPosition + 1) % images.Count;
+                imagesCarousel.Position = actualPosition;
+            }
+
+
+
+
         }
     }
 }
